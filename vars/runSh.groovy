075e58f2ac5821ff78375bb460749f6e087947ee
Jakub <kuba@3sky.dev>
2021-05-26 17:23:11 +0200

    add some fix

--- vars/runSh.groovy
+++ vars/runSh.groovy
@@ -1,11 +1,6 @@
-def call(def context, String content) {
+def call(def context, String command) {
     echo "||------ EXECUTE SOME COMAND -------||"
-    executeCommand(context, content)
-
-}
-
-
-def executeCommand(context, String command) {
-    stdout = script.sh(script: command, returnStdout: true)
+    stdout = sh(script: command, returnStdout: true)
     return stdout.trim()
+
 }

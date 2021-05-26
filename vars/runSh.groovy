5a78158b0b59b68e698c04cca90608dfe4646ba3
Jakub <kuba@3sky.dev>
2021-05-26 17:26:10 +0200

    Exec command

--- vars/runSh.groovy
+++ vars/runSh.groovy
@@ -1,6 +1,7 @@
 def call(def context, String command) {
     echo "||------ EXECUTE SOME COMAND -------||"
-    stdout = sh(script: command, returnStdout: true)
-    return stdout.trim()
+    sh """
+    $command
+    """
 
 }
